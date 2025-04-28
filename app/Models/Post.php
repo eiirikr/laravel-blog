f2cc523f14ece761f0ce48335a3b74e4615b9305
javierjericm <javierjericm@gmail.com>
2025-04-28 20:45:52 +0800

    Added basic frontend scaffolding for authentication

---
+++ app/Models/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function user()
+    {
+        //define an inverse one to many relationship between the Post model and the User model
+        return $this->belongsTo('App\Models\User');
+    }
+}
